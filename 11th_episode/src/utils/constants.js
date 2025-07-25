900dd00f05d2ef4a27eac429a51dee0e41a63cfb
sowmya <isowmyatechie@gmail.com>
2025-07-26 00:16:47 +0530

    11th episode

---
+++ 11th_episode/src/utils/constants.js
@@ -0,0 +1,6 @@
+export const LOGO_URL =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSrM5InRJkHuGm3QE1FdSxga01FCQFBYnjzA&s";
+export const IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
+export const RESTAURANT_API_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=";
